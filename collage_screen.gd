a11cb0bbbb5ffa7e9c57545bd52322be4421505f
Guilherme Buss <guibuss10@gmail.com>
2025-05-30 16:34:07 -0300

    Integrei o collage screen.

--- collage_screen.gd
+++ collage_screen.gd
@@ -4,13 +4,28 @@ extends Node2D
 
 @export var is_dragging = -1
 
+var collageCount = 0
+
+func _process(delta):
+	if collageCount == 0:
+		$confirm.disabled = false
+		
 func _ready():
 	for i in range(3):
+		collageCount += 1
 		spawn_point.progress_ratio = randf()
 		
 		var collage = load('res://collage.tscn').instantiate()
 		collage.num = i
 		collage.global_position = spawn_point.global_position
 		
-		add_child(collage)
+		$collagesGroup.add_child(collage)
+
+func _on_area_collages_exited(body):
+	collageCount -= 1
+	
+	if collageCount < 0:
+		collageCount = 0
 		
+func _on_confirm_pressed():
+	get_parent().collageEnded()
--- main.gd
+++ main.gd
@@ -1,6 +1,25 @@
 extends Node2D
 
+var collageMode = true
 
-func _on_menu_start_game() -> void:
+func _ready():
+	$Menu.show()
+	$Inicio.hide()
+	$collageScreen.hide()
+	
+func _on_menu_start_game():
 	$Menu.hide()
 	$Inicio.show()
+	$collageScreen.show()
+	
+	#pausa o jogo
+	$Inicio.process_mode = Node.PROCESS_MODE_DISABLED
+	
+func collageEnded():
+	$collageScreen.hide()
+	
+	var collages = $collageScreen.get_node('collagesGroup').duplicate()
+	$Inicio.add_child(collages)
+	
+	#despausa
+	$Inicio.process_mode = Node.PROCESS_MODE_INHERIT
