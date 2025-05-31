b36ca8d8daad364a2ec8335d03d43d044160b70e
Pedro Thomaz Conzatti Martins <pedrothomazm@gmail.com>
2025-05-30 21:02:23 -0300

    Ajuste funcionamento dos botões do menu

--- menu.gd
+++ menu.gd
@@ -3,11 +3,11 @@ extends Control
 signal start_game
 
 
-func _on_play_button_up():
+func _on_play_button_pressed():
 	emit_signal("start_game")
 
-func _on_config_button_up():
+func _on_config_button_pressed():
 	pass
 
-func _on_quit_button_up():
+func _on_quit_button_pressed():
 	get_tree().quit()
